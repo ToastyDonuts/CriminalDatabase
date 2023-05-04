68a50a72452798589d36a1f3fa931f551ac50efc
Super <supaseth2008@gmail.com>
2023-05-04 04:03:39 +0000

    Add template

---
+++ main.py
@@ -0,0 +1,10 @@
+class CriminalTemplate:
+    
+    def __init_(self, name, age, offense, sentences, status):
+        self.name = name
+        self.age = age
+        self.offense = offense
+        self.sentences = sentences
+        self.status = status
+    def dicttemp(self):
+        
