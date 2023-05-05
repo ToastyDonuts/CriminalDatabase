745ef62096a97ab6be61cbc48a0202c2f041827c
Super <supaseth2008@gmail.com>
2023-05-05 19:12:42 +0700

    Added login systems, create some part of mainmenu

---
+++ login_system.py
@@ -0,0 +1,74 @@
+import sqlite3
+import bcrypt
+from questionary import prompt
+from collections.abc import Mapping
+
+# Connect to the SQLite database
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
+
+# Create a table for storing user data
+c.execute('''CREATE TABLE IF NOT EXISTS users
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              username TEXT NOT NULL,
+              password TEXT NOT NULL)''')
+
+# Register function
+def register():
+    # Get the username and password from the user
+    username = input('Enter a username: ')
+    password = input('Enter a password: ')
+
+    # Hash the password using bcrypt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    # Insert the user data into the database
+    c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
+    conn.commit()
+
+    # Print a success message
+    print('Registration successful!')
+
+# Login function
+def login():
+    # Get the username and password from the user
+    username = input('Enter your username: ')
+    password = input('Enter your password: ')
+
+    # Get the user data from the database
+    c.execute("SELECT * FROM users WHERE username = ?", (username,))
+    user = c.fetchone()
+
+    # Check if the user exists and the password is correct
+    if user is not None and bcrypt.checkpw(password.encode('utf-8'), user[2]):
+        # Print a success message
+        print('Login successful!')
+
+    # Print an error message if the login fails
+    else:
+        print('Invalid username or password')
+
+# Main loop
+while True:
+    # Get the user's choice
+    loginchoices = [
+
+        {
+            'type': 'select',
+            'message': "Please Sign-In before accessing the database",
+            'name': 'login',
+            "choices": ["Login", "Register", "Quit"]
+        }
+    ]
+
+    choice = prompt(loginchoices)
+
+    # Call the appropriate function based on the user's choice
+    if choice["login"] == 'Register':
+        register()
+    elif choice["login"] == 'Login':
+        login()
+    elif choice["login"] == 'Quit':
+        break
+    else:
+        print('Invalid choice')
--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-def new_dict(varname):
-    globals()[varname] = {}
+from colorama import Fore
+from questionary import prompt
+
 
 def mainmenu():
-    pass
+    mainmenu = [
+
+        {
+            'type': 'select',
+            'message': "Please Sign-In before accessing the database",
+            'name': 'mainmenuchoice',
+            "choices": ["Add or Remove Criminal Data", "Check Criminals Data", "Modify Exisiting Criminal Data", "Exit the database"]
+        }
+    ]
+
+    mainmenuchoices = prompt(mainmenu)
+    if mainmenuchoices["mainmenuchoice"] == 'Add or Remove Criminal Data':
+        pass
+    elif mainmenuchoices["mainmenuchoice"] == 'Check Criminals Data':
+        pass
+    elif mainmenuchoices["mainmenuchoice"] == 'Modify Existing Criminal Data':
+        pass
+    elif mainmenuchoices["mainmenuchoice"] == 'Exit the database':
+        exit()
+    else:
+        print("Invalid choice")
+
+
+def new_dict(varname):
+    globals()[varname] = {}
 
 class CriminalTemplate:
     
@@ -15,3 +40,4 @@ class CriminalTemplate:
     def addingdict(self):
         new_dict()
         
+mainmenu()
